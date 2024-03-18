d761470f7a901cb8e636303696eb6ecdba8b3e50
Yan Carvalhal da Cunha <yan.carvalhal@original.io>
2024-03-18 04:55:05 -0300

    feat: adicionando botão de "limpar" o input para facilitar caso a pessoa queira procurar por outro personagem.

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,20 +1,31 @@
-import { ChangeEvent, FormEvent } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+import { Search, X } from "lucide-react";
 
 import './index.scss'
-import { Search } from "lucide-react";
 
 interface FilterProps {
   handleFilter: (name: string) => void
 }
 
 export function Filter({ handleFilter }: FilterProps) {
+  const [inputFilter, setInputFilter] = useState('')
+
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-    handleFilter(event.target.value)
+    setInputFilter(event.target.value)
   }
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault()
   }
+
+  function handleCloseClick() {
+    setInputFilter('')
+  }
+
+  useEffect(() => {
+    handleFilter(inputFilter)
+  }, [inputFilter])
 
   return (
     <form className="filter" onSubmit={handleSubmit}>
@@ -24,9 +35,22 @@ export function Filter({ handleFilter }: FilterProps) {
         type="text" 
         placeholder="Nome do personagem"
         onChange={handleInputChange}
+        value={inputFilter}
       />
 
-      <Search size={25} />
+      {!inputFilter 
+        ? (
+          <button className="filter__button">
+            <Search size={25} /> 
+          </button>
+        )
+        : (
+          <button className="filter__button" onClick={handleCloseClick}>
+            <X size={25} />
+          </button>
+        )
+      } 
+      
     </form>
   )
 }
